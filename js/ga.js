3cedf4299466c06fc68fe9b8d71a28bc8248ddb1
kiladzedaviti <kiladzedaviti99@gmail.com>
2026-07-29 17:44:16 +0400

    feat(analytics): add Google Analytics 4 (G-GR7R326P6F) on all public pages

---
+++ js/ga.js
@@ -0,0 +1,12 @@
+/* Google Analytics 4 — Hotel Agava */
+(function () {
+  var ID = "G-GR7R326P6F";
+  var s = document.createElement("script");
+  s.async = true;
+  s.src = "https://www.googletagmanager.com/gtag/js?id=" + ID;
+  document.head.appendChild(s);
+  window.dataLayer = window.dataLayer || [];
+  window.gtag = function () { dataLayer.push(arguments); };
+  gtag("js", new Date());
+  gtag("config", ID);
+})();
